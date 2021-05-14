f39b5745f7093681cad1e1916ff1935c1f704d3a
Pavel Veznik <xveznik@fi.muni.cz>
2021-05-14 08:57:00 +0200

    some fixes in fail.cpp

--- fail.cpp
+++ fail.cpp
@@ -4,16 +4,16 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
 	int* arrayHeap, unsigned int arrayHeapLen) {
   int Stack[5];
 
-  Static[100] = 0; 
-  Stack[100] = 0; 
+  Static[0] = 0;
+  Stack[0] = 0;
 
-  for (int i = 0; i <= 5; i++) Stack [i] = 0;
+  for (int i = 0; i <= 4; i++) Stack [i] = 0;
 
   int* array = new int[5];
-  array[100] = 0; 
+  array[0] = 0;
 
-  arrayStack[100] = 0; 
-  arrayHeap[100] = 0; 
+  arrayStack[0] = 0;
+  arrayHeap[0] = 0;
 
   for (unsigned int i = 0; i <= arrayStackLen; i++) {
       arrayStack[i] = 0;
@@ -31,4 +31,3 @@ int main(void) {
   memcheckFailDemo(arrayStack, 5, arrayHeap, 5);
   return 0;
 }
-
